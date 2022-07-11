af1fd62d89998d5deff8e44c761066c798c48b02
Claire Attridge <clairemattridge@gmail.com>
2022-07-11 09:31:24 -0700

    Edits to waypoint doc.

--- waypoint_processing.R
+++ waypoint_processing.R
@@ -1,21 +1,20 @@
-## 
+## SIMRAD waypoint processing
 ## Author: Claire Attridge
 ## Origin date: May 2022 
 
+setwd("C:/Users/Claire/Desktop/MSc/Thesis")
 
-library()
+library(tidyverse)
 library(plotKML)
 library(tidyr)
 
-r <- readGPX("./MSc_data/Data_new/BMSCLOGGERSV2.gpx", waypoints=T, tracks=F, routes=F)
+r <- readGPX("./MSc_data/Data_new/BMSCSITESV3.gpx", waypoints=T, tracks=F, routes=F)
 
-df <- data.frame(matrix(unlist(r), nrow=length(r), byrow=TRUE)) %>% # Turning list into df
-  add_column(ID = c("Lon", "Lat", "Date", "Waypoint", "Shape")) %>% # Setting names for my rows
-  mutate(ID = as.factor(ID))
+df <- r %>%
+  map_df(as_tibble) # Turning list into tibble
 
-wp <- df %>%
-  rownames_to_column() %>% # Necessary when the original df has meaningful row names
-  gather(variable, value, -ID) %>% # Gathering all values by all variables 
-  spread(ID, value) # Spreading out based on the meaningful row names
+
+
+
 
 
