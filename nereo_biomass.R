5c3ade8e07ba7f80a16a8d10e7562a925dd5c05e
Claire Attridge <clairemattridge@gmail.com>
2022-09-20 16:23:45 -0700

    Finalized data GAM model for relationship

--- nereo_biomass.R
+++ nereo_biomass.R
@@ -2,14 +2,21 @@
 ## Author: Claire Attridge
 ## Origin date: September 2022
 
+setwd("C:/Users/Claire/Desktop/MSc/Thesis")
+
+library(tidyverse)
+library(gam)
+
 nereo <- read.csv("./MSc_data/Data_new/nereo_biomass_2022.csv") 
 
+# converting from circumference to diameter
 nereo <- nereo %>%
   mutate(Sub_diam_cm = (Sub_circ_cm/3.14159))
 
-ggplot(data=nereo, aes(x=Sub_diam_cm, y=Biomass_g)) +
+# plotting sub-bulb (cm) to biomass (g) relationship
+ggplot(data=nereo, aes(x=Sub_circ_cm, y=Biomass_g)) +
   geom_point(size=3, shape=21) +
-  geom_smooth(method="lm", formula = y~x, se=F) +
+  geom_smooth(method="gam", formula = y ~ s(x, bs = "cs", k=3), se=F) +
   scale_fill_manual(values=c("green", "orange")) +
   theme_classic() +
   theme(
@@ -18,5 +25,6 @@ ggplot(data=nereo, aes(x=Sub_diam_cm, y=Biomass_g)) +
     axis.title.y = element_text(color="black", size="11", vjust=3)) +
   xlab("Sub bulb (cm)") + ylab("Biomass (g)")
 
-model <- lm(data=nereo, Biomass_g~Sub_diam_cm)
+# generating gam model of sub-bulb (cm) to biomass (g) relationship
+model <- gam(Biomass_g~s(Sub_diam_cm, bs = "cs", k=2), family="poisson", data=nereo)
 summary(model)
