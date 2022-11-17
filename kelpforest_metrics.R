0a95cad3dc5135b429a028bdfdf29fedec6a317d
Claire Attridge <clairemattridge@gmail.com>
2022-11-17 09:23:48 -0800

    Ggplot edits for fig clarity

--- kelpforest_metrics.R
+++ kelpforest_metrics.R
@@ -57,68 +57,77 @@ kelpgrp <- kelp %>%
 
 
 ### Joining the data together ###
-kelpdat <- merge(densgrp, kelpgrp, by = "SiteName", all=TRUE) %>%
-               merge(tempgrp) # Adding in the temp logger data
+kelpdat <- merge(densgrp, kelpgrp, by = "SiteName", all=TRUE)
 
 
 #### Plotting the data ----
 
+tiff(file="./MSc_plots/KelpMetrics3.tiff", height = 5, width = 7, units = "in", res=500)
+
 ## Site specific density (summed Macro & Nereo) # Ordered by increasing density
 d1 <- ggplot() +
-  geom_point(data=dens, size=3, alpha=0.1, aes(x=reorder(SiteName,Kelp), y=Kelp)) +
-  geom_pointrange(data=kelpdat, size=0.7, aes(x=reorder(SiteName,KelpM), y=KelpM, ymin=KelpM-KelpSD, ymax=KelpM+KelpSD)) +
+  geom_point(data=dens, size=2, alpha=0.1, aes(x=reorder(SiteName,Kelp), y=Kelp)) +
+  geom_pointrange(data=kelpdat, size=0.5, aes(x=reorder(SiteName,KelpM), y=KelpM, ymin=KelpM-KelpSD, ymax=KelpM+KelpSD)) +
   scale_y_continuous(limits=c(-0.8,25), breaks=c(0,6,12,18,24)) +
   theme_classic() +
   theme(
-        # axis.text.x = element_text(color="black", size="9.5", angle=45, vjust=0.89, hjust=0.85),
-        axis.text.x = element_blank(),
+        axis.text.x = element_text(color="black", size="9.5", angle=45, vjust=0.89, hjust=0.85),
+        # axis.text.x = element_blank(),
         axis.text.y = element_text(color="black", size="10"),
-        axis.title.y = element_text(color="black", size="11", vjust=3)) +
+        axis.title.y = element_text(color="black", size="12", vjust=1)) +
   xlab("") + ylab("Density (/m2)")
+d1
+
+dev.off()
 
 ## Site specific canopy height # Ordered by increasing density
 c1 <- ggplot() + 
-  geom_point(data=kelpbydens, size=3, alpha=0.1, aes(x=reorder(SiteName,KelpM), y=Height_m)) +
-  geom_pointrange(data=kelpdat, size=0.7, aes(x=reorder(SiteName,KelpM), y=HeightM, ymin=HeightM-HeightSD, ymax=HeightM+HeightSD)) +
+  geom_point(data=kelpbydens, size=2, alpha=0.1, aes(x=reorder(SiteName,KelpM), y=Height_m)) +
+  geom_pointrange(data=kelpdat, size=0.5, aes(x=reorder(SiteName,KelpM), y=HeightM, ymin=HeightM-HeightSD, ymax=HeightM+HeightSD)) +
   # scale_y_continuous(limits=c(-0.4,9), breaks=c(0,2,4,6,8)) +
   theme_classic() +
   theme(
         # axis.text.x = element_text(color="black", size="9.5", angle=45, vjust=0.89, hjust=0.85),
         axis.text.x = element_blank(),
         axis.text.y = element_text(color="black", size="10"),
-        axis.title.y = element_text(color="black", size="11", vjust=3)) +
+        axis.title.y = element_text(color="black", size="12", vjust=1)) +
   xlab("") + ylab("Canopy height (m)") +
   annotate("text", size=4, x=1, y=0.5, label="NA")
 
+
+# kelpbydens <- kelpbydens[-346,] # Removing outlier point at Second Beach South!!
+
+
 ## Site specific biomass # Ordered by increasing density
 b1 <- ggplot() + 
-  geom_point(data=kelpbydens, size=3, alpha=0.1, aes(x=reorder(SiteName,KelpM), y=Biomass_g)) +
-  geom_pointrange(data=kelpdat, size=0.7, aes(x=reorder(SiteName,KelpM), y=BiomassM, ymin=BiomassM-BiomassSD, ymax=BiomassM+BiomassSD)) +
+  geom_point(data=kelpbydens, size=2, alpha=0.1, aes(x=reorder(SiteName,KelpM), y=Biomass_g)) +
+  geom_pointrange(data=kelpdat, size=0.5, aes(x=reorder(SiteName,KelpM), y=BiomassM, ymin=BiomassM-BiomassSD, ymax=BiomassM+BiomassSD)) +
   # scale_y_continuous(limits=c(-0.4,9), breaks=c(0,2,4,6,8)) +
   theme_classic() +
   theme(axis.text.x = element_text(color="black", size="9.5", angle=45, vjust=0.89, hjust=0.85),
         axis.text.y = element_text(color="black", size="10"),
-        axis.title.y = element_text(color="black", size="11", vjust=3)) +
+        axis.title.y = element_text(color="black", size="12", vjust=1)) +
   xlab("") + ylab("Biomass (g)") +
   annotate("text", size=4, x=1, y=0.5, label="NA")
 
+
 ## Grouped multi panel plot ##
 
-tiff(file="./MSc_code/draftplots/KelpMetrics.tiff", height = 10, width = 5.5, units = "in", res=500)
+tiff(file="./MSc_plots/KelpMetrics.tiff", height = 10, width = 7, units = "in", res=500)
 
 ggarrange(d1, c1, b1, ncol=1, align="v", heights=c(1,1,1.4)) # Generating the paneled plot
 
 dev.off() 
 
 
-
 #### Regressions of the data ----
 
 kelpdat2 <- merge(kelpdat, rls_richness, by="SiteName", all=T)
 
-ggplot(kelpdat2, aes(x=HeightM, y=species_richness)) +
+ggplot(kelpdat2, aes(x=HeightM, y=KelpM)) +
   geom_point() +
   # geom_smooth(method="lm", formula = y ~ x + I(x^2), se=T) + # Lm quadratic
   # geom_smooth(method="lm", formula = y ~ x, se=T) + # Lm
-  geom_smooth(method="gam", formula = y ~ s(x, bs="cs", k=3), se=T) + # GAM
-  theme_classic()
+  # geom_smooth(method="gam", formula = y ~ s(x, bs="cs", k=3), se=T) + # GAM
+  theme_classic() +
+  xlab("Kelp Density") + ylab("Kelp Biomass")
